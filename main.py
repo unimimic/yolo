e93ed15dfba05edea9f1d8cade64fbdcbcfff41a
wei <unimimic@gmai.com>
2024-10-13 17:38:59 +0800

    Refactor main.py

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ task = 'train'
 
 if __name__ == "__main__":
   if task == 'train':
-    # Load a model
     model = YOLO("./models/yolo11n.pt") # load a pretrained model (recommended for training)
     # model = YOLO("./models/yolov8n.pt") 
-
-    # Train the model with MPS
     results = model.train(
       data=f"./projects/{projectName}/dataset/data.yaml",
       epochs=100000,
@@ -27,17 +24,13 @@ if __name__ == "__main__":
       device=0, # device=0 mps
       name= taskName,
       project = f'./projects/{projectName}/output',
-      cfg= f"./projects/{projectName}/args.yaml"
+      cfg= f"./cfg/args.yaml"
     )
   elif task == 'export':
-    # Load a model
     model = YOLO(f"./projects/{projectName}/output/{taskName}/weights/best.pt")  # load a custom trained model
-    # Export the model saved_model
     model.export(format="openvino")
     # model.export(format="saved_model")
   elif task == 'valid':
-    # Load a model
     model = YOLO(f"./projects/{projectName}/output/{taskName}/weights/best.pt")
-    # Validate the model
     metrics = model.val()
     print(metrics.box.map)  # map50-95
